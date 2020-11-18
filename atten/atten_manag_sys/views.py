d8937c8f8329e187672bcebe607998632fc7c735
aasthaagarwal <aastha.agarwal_cs18@gla.ac.in>
2020-11-19 01:16:34 +0530

    url and views setup start

--- atten/atten_manag_sys/views.py
+++ atten/atten_manag_sys/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+	return HttpResponse('home page')
+
+
+def contact(request):
+	return HttpResponse('contact page')
+
+
+def rule(request):
+	return HttpResponse('rules')
 
 # Create your views here.
